1cee68bbbad5b196b7f508f5ea62b835ec38b1bd
smalltown <smalltown20110306@gmail.com>
2021-09-23 16:55:33 +0800

    disable sshd

--- modules/sshd/main.tf
+++ modules/sshd/main.tf
@@ -1,8 +1,16 @@
-locals {
-  mask = var.enable ? false : true
+data "ignition_systemd_unit" "sshd_disable" {
+  name = "sshd-disable.service"
+  enabled = ! var.enable
+  content = file("${path.module}/files/services/sshd-disable.service")
 }
 
-data "ignition_systemd_unit" "sshd" {
-  name = "sshd.service"
-  mask = local.mask
+data "ignition_file" "sshd_config" {
+  filesystem = "root"
+  mode       = 420
+
+  path = "/etc/ssh/sshd_config"
+
+  content {
+    content = file("${path.module}/files/configs/sshd_config")
+  }
 }
