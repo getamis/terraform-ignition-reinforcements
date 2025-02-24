4e3e7b45694e5281d08f183419de4f61defbc739
alex.hsu <icewhite@widelab.org>
2025-02-24 12:02:36 +0800

    chore: update terraform-providers-ignition to 2.1.2

--- modules/ecr-credential-provider/versions.tf
+++ modules/ecr-credential-provider/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     ignition = {
       source = "terraform-providers/ignition"
-      version = "= 2.3.1"
+      version = "2.1.2"
     }
     local = {
       source = "hashicorp/local"
