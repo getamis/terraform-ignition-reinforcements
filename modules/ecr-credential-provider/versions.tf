2b50d01664f8035de51be7c046445c9bfc8f4e26
alex.hsu <icewhite@widelab.org>
2025-02-24 11:20:27 +0800

    feat: bump terraform-provider ignition to 2.3.1

--- modules/ecr-credential-provider/versions.tf
+++ modules/ecr-credential-provider/versions.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     ignition = {
       source = "terraform-providers/ignition"
+      version = "== 2.3.1"
     }
     local = {
       source = "hashicorp/local"
--- modules/locksmithd/version.tf
+++ modules/locksmithd/version.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     ignition = {
       source  = "community-terraform-providers/ignition"
-      version = "~> 2.0"
+      version = "== 2.3.1"
     }
   }
 }
